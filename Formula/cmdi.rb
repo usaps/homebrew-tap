088ce71f6d925a03c58561e84996913a4436adf6
Jero <jero@eggmotion.es>
2026-09-12 13:16:29 +0200

    bump: cmdi 0.1.20

--- Formula/cmdi.rb
+++ Formula/cmdi.rb
@@ -1,13 +1,13 @@
 class Cmdi < Formula
   desc "High-speed, intelligent media offloading for film sets"
   homepage "https://github.com/usaps/CMDI"
-  version "0.1.19"
+  version "0.1.20"
   license "MIT"
 
   # macOS universal binary (Apple Silicon + Intel)
   # Source code is private; binaries distributed from public releases repo
-  url "https://github.com/usaps/cmdi-releases/releases/download/v0.1.19/cmdi"
-  sha256 "e740c0f5c93719ecbcf69be348d698ded6c0b759ec784af2fb4d775154cf9b84"
+  url "https://github.com/usaps/cmdi-releases/releases/download/v0.1.20/cmdi"
+  sha256 "5b2dfdea70979c1f0cb8a8b538294c495b780961a39191444f186e4f81cbd731"
 
   def install
     bin.install "cmdi"
